ef1698bf6ff44527f8ee1c3159dc2b822b21e049
Casandra Cain <cas@cascain.com>
2026-06-30 13:26:03 -0400

    fix: use named wildcard route for Express 5 compatibility

--- server/server.js
+++ server/server.js
@@ -629,7 +629,7 @@ app.get('/ready', async (req, res) => {
 if (env.NODE_ENV === 'production') {
   const distPath = path.join(__dirname, '../dist');
   app.use(express.static(distPath));
-  app.get('*', (req, res) => {
+  app.get('/{*path}', (req, res) => {
     res.sendFile(path.join(distPath, 'index.html'));
   });
 }
